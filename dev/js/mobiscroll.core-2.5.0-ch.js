09418389ff1564ef2240d4c3e34c226485c4d28f
apple <apple@Creem-PingAn.local>
2015-09-25 10:12:55 +0800

    移动端日期插件

---
+++ dev/js/mobiscroll.core-2.5.0-ch.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lixiaoxi on 15/9/7.
+ * @description
+ */
+(function ($) {
+    $.mobiscroll.i18n.ch = $.extend($.mobiscroll.i18n.ch, {
+        setText: '确定',
+        cancelText: '取消'
+    });
+})(jQuery);
---
+++ dev/js/mobiscroll.datetime-2.5.0-ch.js
@@ -0,0 +1,26 @@
+(function($) {
+    $.mobiscroll.i18n.ch = $.extend($.mobiscroll.i18n.ch || {}, {
+        dateFormat: 'yy年mm月dd日',
+        dateOrder: 'yymmdd',
+        timeWheels: 'hhiiA',
+        timeFormat: 'hh:ii A',
+        monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        dayNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+        dayNamesShort: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+        shortYearCutoff: '+10',
+        //monthText: '月',
+        //dayText: '日',
+        //yearText: '年',
+        //hourText: '小时',
+        //minuteText: '分钟',
+        //secText: '秒',
+        ampmText: '&nbsp;',
+        nowText: '今天',
+        showNow: true,
+        stepHour: 1,
+        stepMinute: 1,
+        stepSecond: 1,
+        separator: ' '
+    });
+})(jQuery);
